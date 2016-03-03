6252d126500d319ada2d16ddf8e75c58d1e3eece
jlevinpool <poolj@onid.oregonstate.edu>
2016-03-02 22:10:57 -0800

    more script testing

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', bindTestButton);
 
 function bindTestButton() {
 	document.getElementById('testButton').addEventListener('click', function(event){
+		document.getElementById('testSpan').textContent = "It Worked!";
 		console.log("Success!");
 		event.preventDefault();
 	});
