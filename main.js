5905e8e5feab859b84b6d425d7d861272004ceaf
jlevinpool <poolj@onid.oregonstate.edu>
2016-03-06 21:06:31 -0800

    Added input form

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ app.get('/load-table',function(req,res,next){
 /* Provided Reset Table Routine */
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+  mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ 
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
