c5dc181bdb8058b2cecdb075121799630ddbe3e2
jlevinpool <poolj@onid.oregonstate.edu>
2016-03-11 10:59:41 -0800

    Added buttons
    Initial delete test

--- public/scripts/dbFormSetup.js
+++ public/scripts/dbFormSetup.js
@@ -28,3 +28,25 @@ Date.prototype.toDateInputValue = (function() {
 function setCurrentDate() {
 	document.getElementById('inputDate').value = new Date().toDateInputValue();
 }
+
+/* Delete Table Row adapted from: http://jsfiddle.net/GRgMb/ */
+function deleteRow(tableID,currentRow) {
+    try {
+        var table = document.getElementById(tableID);
+        var rowCount = table.rows.length;
+        for (var i = 0; i < rowCount; i++) {
+            var row = table.rows[i];
+            if (row==currentRow.parentNode.parentNode) {
+                if (rowCount < 1) {
+                    alert("Cannot delete row from empty table.");
+                    break;
+                }
+                table.deleteRow(i);
+                rowCount--;
+                i--;
+            }
+        }
+    } catch (e) {
+        alert(e);
+    }
+}
