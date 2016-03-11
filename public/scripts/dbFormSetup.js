a03c3ff814a72cca5c9ce6bcbc96b999d3b19f9f
jlevinpool <poolj@onid.oregonstate.edu>
2016-03-11 15:57:59 -0800

    fixed typo

--- public/scripts/dbFormSetup.js
+++ public/scripts/dbFormSetup.js
@@ -31,8 +31,7 @@ function bindTestButton() {
 					else {
 						/* Insert adapted from: http://stackoverflow.com/questions/18333427/how-to-insert-row-in-html-table-body-in-javascript */
 						var table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
-						console.log(table);
-						var newRow = table.insertRow(table.row.length);
+						var newRow = table.insertRow(table.rows.length);
 						table.appendChild(newRow);
 						
 					}
